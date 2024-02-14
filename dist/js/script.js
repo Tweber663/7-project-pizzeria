a746c3584cd8d0cf09b0612c8d737c70e0428c56
Thomas Weber <tweber.1995@hotmail.com>
2024-02-14 16:21:43 +0100

    JS - Active class deleted

--- dist/js/script.js
+++ dist/js/script.js
@@ -54,44 +54,56 @@ const select = {
 
   class Product {
     constructor(id, data) {
-      const thisProduct = this;
-      thisProduct.id = id; 
+      const thisProduct = this; 
+      thisProduct.id = id;
       thisProduct.data = data;
-      
+      thisProduct.renderInMenu();
     }
+      renderInMenu() { // Responsbile for rendeing information on our website
+        const thisProduct = this; 
+
+        /*generating HTML based on template*/
+        const generateHTML = templates.menuProduct(thisProduct.data);
+        
+        /* create element using utils.createElementFromHtml 'functions.js'*/
+        thisProduct.element = utils.createDOMFromHTML(generateHTML);
+
+        /* find menu container */ 
+        const menuContainer = document.querySelector(select.containerOf.menu);
+
+        /* add element to menu*/ 
+        menuContainer.appendChild(thisProduct.element);
+      }
+  
   }
 
-
   const app = {
-    initMenu: function() {
+    initMenu() {  // <-- Cycling through each 'product' inside 'dataSource'  
       const thisApp = this;
-      console.log('thisApp.data', thisApp.data)
+      console.log('this app data', thisApp.data);
+      for(let productData in thisApp.data.products) {  
+        // Createting a new object based on the cycled info, 1st 'product value, 2nd, passed the 'product' object property
+       new Product(productData, thisApp.data.products[productData])
+      }
+    }, 
 
-      for(let productData in thisApp.data.products) {                //Cycles throught each product inside the data.js dataSource Object
-       new Product(productData, thisApp.data.products[productData]) // Pushes each product into 'product class'
-      }                                                             //1st argu.. grabs propety names, 2nd argu.. grabs the values. 
-
-      const testProduct = new Product();
-      console.log('test product', testProduct);
+    initData() {  // ---> 💾 connecting to data.js * grabbing 'dataSource' object
+      const thisApp = this;
+      thisApp.data = dataSource; 
     },
 
-    initData: function() {
+    init() {
       const thisApp = this; 
-      thisApp.data = dataSource;
-    },
-
-    init: function(){
-      const thisApp = this;
-      console.log('*** App starting ***');
+      console.log('***App starting***');
       console.log('thisApp:', thisApp);
-      console.log('classNames:', classNames);
-      console.log('settings:', settings);
-      console.log('templates:', templates);
-      thisApp.initData();
-      thisApp.initMenu();
-    },
-  };
+      console.log('classNmaes', classNames);
+      console.log('settings', settings);
+      console.log('templates', templates);
+      
+      thisApp.initData(); // 1st 
+      thisApp.initMenu(); // 2nd 
+    }
+   }
 
-
-  app.init();
-}
+   app.init();
+  }
