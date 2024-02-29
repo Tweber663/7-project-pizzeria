746b5f4ef901932ec8b81dad0bee55c0caf59175
Thomas Weber <tweber.1995@hotmail.com>
2024-02-29 19:01:42 +0100

    JS - end day, save progress

--- dist/js/script.js
+++ dist/js/script.js
@@ -97,6 +97,7 @@
       thisProduct.initOrderForm();
       thisProduct.initAmountWidget();
       thisProduct.processOrder();
+
     }
       renderInMenu() { // Responsbile for rendeing information on our website
         const thisProduct = this; 
@@ -185,6 +186,7 @@
         event.preventDefault();
 
         thisProduct.addToCart(); /*triggering a function */
+        thisProduct.prepareCartProductParams()
         thisProduct.processOrder();
       })
       }
@@ -194,23 +196,62 @@
         const thisProduct = this;
 
         /*brief summary of entire product that contains 'only' nedded info*/ 
-        productSUmmary = [];
+        const productSummary = {
+          id: thisProduct.id,
+          name: thisProduct.data.name,
+          amount: thisProduct.amountWidget.value,
+          priceSingle: thisProduct.priceSingle, 
+          price: thisProduct.priceSingle * thisProduct.amountWidget.value,
+          params: '',
+        };
+        return productSummary;
+      }
+
+      /*2.Preparing cart elem befor they are added*/
+      prepareCartProductParams() {
+        const thisProduct = this;
+        console.log('id', thisProduct.data);
+        /*this thing is hooked up to a form, an
+        d tranfroms selected items into an object*/
+        const formData = utils.serializeFormToObject(thisProduct.dom.form);
+
+
+       /*First we cycle throught extras 'CATEGORY', source, crust etc..*/
+       /*[thisProduct.data.par..] All Objects {sauce: {…}, toppings: {…}, crust: {…}}*/
+       /*[paramID] individual object names ONLY*/ 
+        for (let paramId in thisProduct.data.params) {
+        /*using the object name, on the object, to acess it and all it's info */
+          const paramObject = thisProduct.data.params[paramId];
+          console.log(paramObject);
+          
+          /*Cycling throught individual toppings following the logic above */
+          for (let toppingId in paramObject.options) {
+
+            console.log(paramObject.options[toppingId]);
+          }
+         
+
+        }
+
+        // const selected = {
+        //   ingredients: thisProduct.data.params.ingredients.label,
+        // }
+
 
       }
       
-
       /*2.Adding elements to our'cart' CLASS*/
       addToCart() {
         const thisProduct = this; 
-
         /*We're triggering our Cart class + passing a refrence / instant*/
-        app.cart.add(thisProduct);   
+        app.cart.add(thisProduct.prepareCartProduct());   
       }
 
       processOrder() {
         const thisProduct = this;
         //Converting FORM to object strocture e.g {sauce: 'tomato etc]}
         const formData = utils.serializeFormToObject(thisProduct.dom.form);
+        console.log('form Data:', formData);
   
         //set price to default amount
         let price = thisProduct.data.price
@@ -259,8 +300,10 @@
       }
 
       /* Responbile multiplaying the price bsed on quantity amount (widget) */
-      console.log(thisProduct.amountWidget.value);
       price*= thisProduct.amountWidget.value;
+      
+      /*STATIC - product base price (without any extras)*/
+      thisProduct.priceSingle = thisProduct.data.price;
 
       //update calculate price in the HTML 
       thisProduct.dom.priceElem.innerHTML = price;
@@ -277,7 +320,6 @@ class AmountWidget {
     /*Widget starting value*/
 
     thisWidget.input.value? thisWidget.setValue(thisWidget.input.value) : thisWidget.setValue(settings.amountWidget.defaultValue);
-
     thisWidget.initActions(); 
   }
 
@@ -294,19 +336,17 @@ class AmountWidget {
   }
 
   setValue(value) {
-    const thisWidget = this; 
-    
-    /*changes input value from string to num */
+    const thisWidget = this;
+    console.log(value);
     const newValue = parseInt(value);
-
-   /* TODO: Add validation */
-    if(thisWidget.value !== newValue && !isNaN(newValue) && newValue > settings.amountWidget.defaultMin && newValue <= settings.amountWidget.defaultMax) {
-        thisWidget.value = newValue;
-        thisWidget.announce();
+    if (thisWidget.value !== newValue && !isNaN(newValue) && newValue >= settings.amountWidget.defaultMin && newValue <= settings.amountWidget.defaultMax) {
+    thisWidget.value = newValue;
+    thisWidget.announce();
     }
-    thisWidget.input.value = thisWidget.value
+    thisWidget.input.value = thisWidget.value;
+    console.log(thisWidget.input.value)
+    }
 
-  }
 
   initActions() {
     const thisWidget = this; 
